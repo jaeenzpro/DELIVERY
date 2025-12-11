2b23d0a2dcc8f4f57b3d3ce88c1694e6a1abcdc7
jaeenzpro <jaeenzh@gmail.com>
2025-12-11 02:20:15 -0500

    Delete PROYECTO_FINAL directory

--- PROYECTO_FINAL/reclamos.php
+++
@@ -1,9 +0,0 @@
-﻿<?php if (isset($_GET["boleta"])): ?>
-<script>
-document.addEventListener("DOMContentLoaded", () => {
-    const modalBoleta = new bootstrap.Modal(document.getElementById("boletaReclamoModal"));
-    modalBoleta.show();
-});
-</script>
-<?php endif; ?>
-
